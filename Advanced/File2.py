dd896960787ed86e7b9264c4ccb80be78815029a
laiba41 <146206954+laiba41@users.noreply.github.com>
2023-10-22 03:11:42 -0700

    Add Advanced

---
+++ Advanced/File2.py
@@ -0,0 +1,8 @@
+# Built-In Object Types
+number = 34274
+string = 'laiba'
+list = ['name', 'number', 21]
+tuple = ('name', 'number', 21)
+dictionary = {'name': string, 'number': number, 'age': 21}
+file = open('Advanced/python.txt', 'r')
+print(file.read())
