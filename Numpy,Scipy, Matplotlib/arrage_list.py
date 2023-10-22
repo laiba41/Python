92c04eed11e3425b2202ed2269c4838d14c4b770
laiba41 <146206954+laiba41@users.noreply.github.com>
2023-10-22 03:43:52 -0700

    Add Numpy,Matplotlib

---
+++ Numpy,Scipy, Matplotlib/arrage_list.py
@@ -0,0 +1,16 @@
+# Import Libraries
+import numpy as np
+
+# Arrange list using np
+
+a = np.arange(4, 7)
+print(a)
+print(a.shape)
+
+b = np.arange(1, 125, 24)
+print(b)
+print(b.shape)
+
+c = np.arange(134, 32, -22)
+print(c)
+print(c.shape)
